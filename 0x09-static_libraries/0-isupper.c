06fd41757198c7b1964e4e887a31182d8883729a
AMahadai <somebiz777@gmail.com>
2023-09-02 14:53:25 -0700

    Aayaheyna

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
  * _isupper - uppercase letters
  * @c: char to check
  *
  * Return: 0 or 1
  */
-int _isupper(char c)
+
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1); ielse
-	return (0);
+		return (1);
+	else
+		return (0);
 }
